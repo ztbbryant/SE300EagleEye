95eba5743e9e628f1f66dea5e5616b4a0e279381
Marcell <marcellgilliam@outlook.com>
2017-03-06 15:27:24 -0500

    Main Activity and Fragments .java added

---
+++ app/src/main/java/com/example/zacharybryant/eagleeyeapp/FirstFragment.java
@@ -0,0 +1,23 @@
+package com.example.zacharybryant.eagleeyeapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;import com.example.user.navigationdrawer.R;
+
+/**
+ * Created by user on 12/31/15.
+ */
+public class FirstFragment extends Fragment{
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.first_layout, container, false);
+        return myView;
+    }
+}
--- app/src/main/java/com/example/zacharybryant/eagleeyeapp/MainActivity.java
+++ app/src/main/java/com/example/zacharybryant/eagleeyeapp/MainActivity.java
@@ -10,8 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    //AlexisBertholf
 }
+
+//AlexisBertholf
 //Zach Bryant
 //Shawn McDonnell
 //Marcell Gilliam
