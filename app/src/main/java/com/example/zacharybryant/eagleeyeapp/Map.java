6159dc4f7793601bc72b14f0034b853e18b983c7
Zach Bryant <ztbbryant@gmail.com>
2017-04-25 18:25:32 -0400

    Added framework for map methods

---
+++ app/src/main/java/com/example/zacharybryant/eagleeyeapp/Map.java
@@ -0,0 +1,21 @@
+package com.example.zacharybryant.eagleeyeapp;
+
+import android.os.Build;
+
+/**
+ * Created by Zachary Bryant on 4/25/2017.
+ */
+
+public class Map {
+
+    public void addPin(long lat, long lon, String pinName)
+    {
+        //TODO Create logic to add pins to the map
+    }
+
+    public void mapStart()
+    {
+        //TODO Add map starting location
+    }
+
+}
